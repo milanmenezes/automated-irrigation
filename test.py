da224c54f152ae2059bab40fa5c4c16ee8ddbbfb
milanmenezes <milanmenezes@gmail.com>
2018-11-22 12:20:08 +0530

    trying to use gsm module

---
+++ test.py
@@ -0,0 +1,9 @@
+import serial
+import os, time
+# Enable Serial Communication
+port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+# Transmitting AT Commands to the Modem 
+# '\r\n' indicates the Enter key
+port.write('AT'+'\r\n')
+rcv = port.read(10)
+print rcv
